9a2001f5884b2ea65b3becb9c206b2cddc327feb
schasins <schasins@gmail.com>
2015-09-15 13:55:23 -0700

    Sketching out the infrastructure needed for NN training.

--- src/scripts/background/mainpanel_script.js
+++ src/scripts/background/mainpanel_script.js
@@ -2,7 +2,7 @@ var all_script_results = [];
 function setUp(){
 
   //messages received by this component
-  //utilities.listenForMessage("content", "mainpanel", "selectorAndListData", processSelectorAndListData);
+  utilities.listenForMessage("content", "mainpanel", "newTrainingData", handleNewTrainingData);
   
   //messages sent by this component
   //utilities.sendMessage("mainpanel", "content", "startProcessingList", "");
@@ -13,7 +13,57 @@ function setUp(){
     chrome.tabs.create({ url: urls[i] });
   }
 
-
 }
 
 $(setUp);
+
+var trainingData = {};
+
+function handleNewTrainingData(data){
+	var tabId = data.tab_id; // TODO: Fix this.  In future if we get messages from multiple frames in a single tab, we might overwrite existing training data from this tab.
+	trainingData[tabId] = data.data;
+	console.log(trainingData);
+
+	// decide on a set of features to use
+	// turn all tabs' datasets into features in that system -- maybe put a func in the utilities to do this
+	// make net with correct vec length
+	// train net
+	// send net and set of acceptable features to the content scripts
+}
+
+function makeNeuralNet(inputVectorLength, outputNumClasses){
+	// specifies a 2-layer neural network with one hidden layer of 20 neurons
+	var layer_defs = [];
+	// input layer declares size of input. here: 2-D data
+	// ConvNetJS works on 3-Dimensional volumes (sx, sy, depth), but if you're not dealing with images
+	// then the first two dimensions (sx, sy) will always be kept at size 1
+	layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:inputVectorLength});
+	// declare 20 neurons, followed by ReLU (rectified linear unit non-linearity)
+	layer_defs.push({type:'fc', num_neurons:20, activation:'relu'}); 
+	// declare the linear classifier on top of the previous hidden layer
+	layer_defs.push({type:'softmax', num_classes:outputNumClasses});
+
+	var net = new convnetjs.Net();
+	net.makeLayers(layer_defs);
+	return net;
+}
+
+function makeTrainer(net){
+	var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.01, l2_decay:0.001});
+	return trainer;
+}
+
+// inputArray is an array of floats
+// outputClass is an integer from 0 to max class index
+function train(trainer, inputArray, outputClass){
+	var x = new convnetjs.Vol(inputArray);
+	trainer.train(x,outputClass);
+}
+
+function serializeNet(net){
+	// network outputs all of its parameters into json object
+	var json = net.toJSON();
+	// the entire object is now simply string. You can save this somewhere
+	var str = JSON.stringify(json);
+	return str;
+}
--- src/scripts/content/content_script.js
+++ src/scripts/content/content_script.js
@@ -84,9 +84,21 @@ function highlightNode(target, time) {
   return idName;
 }
 
+var thisPageHasBeenLabeledByHand = false;
+
 function processLabelingClick(node){
+  thisPageHasBeenLabeledByHand = true;
   node.__label__ = true;
   console.log(textNodes);
+  utilities.sendMessage("content", "mainpanel", "newTrainingData", {data: makeFeatureLabelPairs(textNodes)});
+}
+
+function makeFeatureLabelPairs(nodeList){
+  var pairs = [];
+  for (var i = 0; i < nodeList.length; i++){
+    pairs.push([nodeList[i].__features__, nodeList[i].__label__]);
+  }
+  return pairs;
 }
 
 function getFeatures(node){
@@ -123,7 +135,21 @@ function processTextNodes(){
 
 setTimeout(processTextNodes, 2000);
 
+function reproduceNet(serializedNet){
+  var json = JSON.parse(serializedNet); // creates json object out of a string
+  var net = new convnetjs.Net(); // create an empty network
+  net.fromJSON(json); // load all parameters from JSON
+  return net;
+}
 
+// inputArray is an array of floats
+function classify(net, inputArray){
+  var x = new convnetjs.Vol(inputArray);
+  var prob = net.forward(x); 
+  // TODO: right now just returning true if it's in the one 'true' category that the current user interaction allows
+  // later we'll want to allow users to show many different categories, so we'll need to test all possible categories for highest probability
+  return prob.w[1] > prob.w[0];
+}
 
-
-
+// accept feature set and serialized neural net from mainpanel
+// run the net on all our textNodes here
